46c5c80864051dcfbc3830cf3f103b33d5ab63cc
Garv0816 <garv020596@gmail.com>
2026-06-06 12:59:02 +0530

    express server

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use("/test",(req , res)=>{
+    res.send("hello from server")
+})
+app.use("/hello",(req , res)=>{
+    res.send("hello 222222 from server")
+})
+
+app.listen(3000, function(){console.log("server is runninng at port number 3000")})
